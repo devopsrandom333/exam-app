4aac4e1d9ac558639b19bf3d7a83e4315a386b67
devopsrandom333 <devopsrandom333@gmail.com>
2018-11-28 18:45:12 +0100

    test

--- src/main/java/com/example/herokupipeexample/CustomerController.java
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +20,17 @@ import static com.codahale.metrics.MetricRegistry.name;
 public class CustomerController {
 
     private CustomerRepository customerRepository;
+    private final Counter postReqs;
 
     @Autowired
-    public CustomerController(CustomerRepository customerRepository ,MetricRegistry registry) {
-      this.customerRepository = customerRepository;
+    public CustomerController(CustomerRepository customerRepository, MetricRegistry registry) {
+        this.customerRepository = customerRepository;
         this.registry = registry;
+        postReqs =  registry.counter(name(Customer.class, "postReqs"));
     }
+
     private final MetricRegistry registry;
+
     @RequestMapping("/")
     public String welcome() {
         registry.meter("Root entrypoint").mark();
@@ -34,20 +39,31 @@ public class CustomerController {
     }
 
     @RequestMapping("/list")
-    public List<Customer> find(@RequestParam(value="lastName") String lastName) {
+    public List<Customer> find(@RequestParam(value = "lastName") String lastName) {
+        Timer responses = registry.timer(name(Customer.class, "responses"));
+
+        final Timer.Context context = responses.time();
         registry.meter("List entrypoint").mark();
+        //doing this just to show you can get the size, easily
+        customerRepository.save(new Customer("test", "test"));
         List<Customer> results = customerRepository.findByLastName(lastName);
         final Histogram resultCounts = registry.histogram(name(Customer.class, "result-counts"));
         resultCounts.update(results.size());
-        return  results;
+        //deleting it again to make  not spam the db
+        customerRepository.delete(customerRepository.findByLastName("test").get(0));
+        try {
+            return results;
+        } finally {
+            context.stop();
+        }
     }
 
     @PostMapping("/")
-    	Customer newCustomer(@RequestBody Customer customer) {
-        System.out.println(customer);
+    Customer newCustomer(@RequestBody Customer customer) {
+        postReqs.inc();
         registry.meter("Create user entrypoint").mark();
 
         return customerRepository.save(customer);
-    	}
+    }
 
 }
